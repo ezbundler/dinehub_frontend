817b1fe204e258a3ac050766f930389b9a07512e
ezbundler <developer.akash1236@gmail.com>
2024-09-14 10:21:39 +0530

    full project commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Akash</div>
+          <div className="right">
+            <p>Street no.4 Scheme 78, Indore(M.P),452010</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Akash Chaudhary</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Akash Chaudhary.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
